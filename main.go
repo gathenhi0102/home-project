e85fe6518750fc6bda3a1f484602a19a22f699de
van long nguyen <long.nguyenvan95@gmail.com>
2020-08-20 22:59:36 +0700

    Init

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	connectionString := os.Getenv("DB_PRIVACY_CONNECTION")
+	fmt.Println(connectionString)
+}
